f5d1778197ecbb5f08483eb8b2f70082ae49c8a2
Stead17 <EVERY-BODY97@mail.ru>
2016-04-20 20:47:41 +0300

    Working on grammar analyzer

--- LexicalAnalyzer/gramm_analyzer.cpp
+++ LexicalAnalyzer/gramm_analyzer.cpp
@@ -25,32 +25,46 @@ void gram_analyze(std::vector<std::vector<std::string>>& vect) {
         printf("\n");
         std::string str = tmp_str[0];
         if (segment_check_flag) {
-            for (int j = 0; j < offset.size(); ++j) {
+            for (int j = i; j < offset.size(); ++j) {
                 std::vector<std::string> tmp = vect[j];
                 file << "000" << std::hex << offset[j] << "\t";
                 copy(tmp.begin(), tmp.end(), std::ostream_iterator<std::string>(file, "\t"));
                 file << "\n";
                 
             }
+            // change this!!
+            if (i < 8)
+                i += offset.size();
+            else
+                i++;
         } else if(str.compare("ASSUME") == 0) {
             file << "\t";
             copy(tmp_str.begin(), tmp_str.end(), std::ostream_iterator<std::string>(file, "\t"));
             file << "\n";
             if ((i + 1) <= vect.size())
                 i++;
+        } else if(label_check(vect[i])){
+            file << "000" << std::hex << offset[offset.size() - 1] << "\t";
+            copy(tmp_str.begin(), tmp_str.end(), std::ostream_iterator<std::string>(file, "\t"));
+            file << "\n";
+            if ((i + 1) <= vect.size())
+                i++;
         } else {
-            tmp_str.clear();
+            command_run(vect[i], offset, i);
+            file << "00" << std::hex << offset[offset.size() - 1] << "\t";
+            copy(tmp_str.begin(), tmp_str.end(), std::ostream_iterator<std::string>(file, "\t"));
+            file << "\n";
             if ((i + 1) <= vect.size())
                 i++;
         }
-        offset.clear();
+//        offset.clear();
     }
     
     file.close();
 }
 
-bool segment_check(std::vector<std::vector<std::string>>& vector, std::vector<std::string>& usr_id, std::vector<int>& offset, int& index) {
-    
+// Maybe add index int&
+bool segment_check(std::vector<std::vector<std::string>>& vector, std::vector<std::string>& usr_id, std::vector<int>& offset, int index) {
     if (valid_segment_name(vector[index])) {
         offset.push_back(0);
         offset.push_back(0);
@@ -63,8 +77,6 @@ bool segment_check(std::vector<std::vector<std::string>>& vector, std::vector<st
         }
         
     } else {
-        if ((index + 1) < vector.size())
-            index++;
         return false;
     }
     
@@ -92,7 +104,7 @@ bool id_check(std::string str) {
     }
 }
 
-bool mnem_check(std::vector<std::string>& vector, std::vector<std::string>& usr_id, std::vector<int>& offset, int& indx) {
+bool mnem_check(std::vector<std::string>& vector, std::vector<std::string>& usr_id, std::vector<int>& offset, int indx) {
     std::string type_array[] = {"DB", "DD", "DW"};
     int last_elem_size = offset[offset.size() - 1];
     if (vector.size() >= 3) {
@@ -116,4 +128,103 @@ bool mnem_check(std::vector<std::string>& vector, std::vector<std::string>& usr_
     }
     
     return false;
+}
+
+// Check if the row is label
+bool label_check(std::vector<std::string> label) {
+    if (!label[1].compare(":") && id_check(label[0]))
+        return true;
+    return false;
+}
+
+void command_run(std::vector<std::string> instuct, std::vector<int>& offset, int indx) {
+    std::string instruct_name = instuct[0];
+    int last_elem_size = offset[offset.size() - 1];
+    if (!instruct_name.compare("MOV"))
+        mov_command(instuct, offset, indx);
+    else if(!instruct_name.compare("ADD"))
+        add_command(instuct, offset, indx);
+    else if (!instruct_name.compare("AND"))
+        and_command(instuct, offset, indx);
+    else if(!instruct_name.compare("OR"))
+        or_command(instuct, offset, indx);
+    else if (!instruct_name.compare("STOS"))
+        stos_command(instuct, offset, indx);
+    else if (!instruct_name.compare("WAIT"))
+        offset.push_back(last_elem_size + 1);
+    else if(!instruct_name.compare("XCHG"))
+        xchg_command(instuct, offset, indx);
+    else if(!instruct_name.compare("RETN"))
+        offset.push_back(last_elem_size + 1);
+    
+}
+
+void mov_command(std::vector<std::string> instr, std::vector<int>& offset, int indx) {
+    auto it = lexems.find(instr[1]);
+    int last_elem_size = offset[offset.size() - 1];
+    if (it != lexems.end()) {
+        std::string tmp = it->second;
+        auto found = tmp.find("REGISTR 16-BIT");
+        if (found != std::string::npos) {
+            offset.push_back(last_elem_size + 4);
+        } else {
+            offset.push_back(last_elem_size + 2);
+        }
+    }
+}
+
+void add_command(std::vector<std::string> instr, std::vector<int>& offset, int indx) {
+    auto it = lexems.find(instr[1]);
+    int last_elem_size = offset[offset.size() - 1];
+    if (it != lexems.end()) {
+        std::string tmp = it->second;
+        auto found = tmp.find("REGISTR 16-BIT");
+        if (found != std::string::npos)
+            // For 16 bit registre
+            offset.push_back(last_elem_size + 3);
+        else
+            // For 8 bit registre
+            offset.push_back(last_elem_size + 2);
+    }
+}
+
+void and_command(std::vector<std::string> instr, std::vector<int>& offset, int indx) {
+    // And is 2 byte in offset
+    // For db user_id to offset 7
+    // For dw user_id to offset 8
+    // For db user_id[] to offset 6
+    // For dw user_id[] to offset 7
+    // For db ds: to offset 4
+    // For dw ds: to offset 4
+    int last_elem_size = offset[offset.size() - 1];
+    auto it = lexems.find(instr[0]);
+    for (int i = 1; i < lexems.size(); ++i) {
+        it = lexems.find(instr[i]);
+    }
+    if (it != lexems.end()) {
+        std::string tmp = it->second;
+        auto found_one = tmp.find("REGISTR 16-BIT");
+        auto found_two = tmp.find("REGISTR 8-BIT");
+        if (found_one != std::string::npos) {
+            
+        } else if(found_two != std::string::npos) {
+            
+        }
+    }
+}
+
+void or_command(std::vector<std::string> instr, std::vector<int>& offset, int indx) {
+    
+}
+
+void stos_command(std::vector<std::string> instr, std::vector<int>& offset, int indx) {
+    
+}
+
+void xchg_command(std::vector<std::string> instr, std::vector<int>& offset, int indx) {
+    
+}
+
+void jbe_command(std::vector<std::string> instr, std::vector<int>& offset, int indx) {
+    
 }
--- LexicalAnalyzer/gramm_analyzer.hpp
+++ LexicalAnalyzer/gramm_analyzer.hpp
@@ -12,13 +12,27 @@
 #include "stdafx.h"
 #include "common_data.h"
 
+enum instructions {MOV, ADD, AND, OR, XCHG, JBE, WAIT, RETN, STOS};
+
 void gram_analyze(std::vector<std::vector<std::string>>&);
 
-bool segment_check(std::vector<std::vector<std::string>>& vector, std::vector<std::string>& usr_id, std::vector<int>& offset, int& index);
-bool mnem_check(std::vector<std::string>& vector, std::vector<std::string>& usr_id, std::vector<int>& offset, int& indx);
+bool segment_check(std::vector<std::vector<std::string>>& vector, std::vector<std::string>& usr_id, std::vector<int>& offset, int index);
+bool mnem_check(std::vector<std::string>& vector, std::vector<std::string>& usr_id, std::vector<int>& offset, int indx);
+void command_run(std::vector<std::string> instuct, std::vector<int>& offset, int indx);
 
 bool id_check(std::string str);
 bool valid_segment_name(std::vector<std::string> sentence);
+bool label_check(std::vector<std::string> label);
+
+
+void mov_command(std::vector<std::string> instr, std::vector<int>& offset, int indx);
+void add_command(std::vector<std::string> instr, std::vector<int>& offset, int indx);
+void and_command(std::vector<std::string> instr, std::vector<int>& offset, int indx);
+void or_command(std::vector<std::string> instr, std::vector<int>& offset, int indx);
+void stos_command(std::vector<std::string> instr, std::vector<int>& offset, int indx);
+void xchg_command(std::vector<std::string> instr, std::vector<int>& offset, int indx);
+void jbe_command(std::vector<std::string> instr, std::vector<int>& offset, int indx);
+
 
 
 #endif /* gramm_analyzer_hpp */
